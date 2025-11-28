839da06a17855508f043377f84a21d0c357d780e
Shourya Rawat <154581170+Rawat-OpenSource@users.noreply.github.com>
2025-11-28 09:11:39 +0100

    Update __init__.py
    bug fix

--- qwen_energy_experiment/src/models/__init__.py
+++ qwen_energy_experiment/src/models/__init__.py
@@ -1,6 +1,6 @@
 """Model management and inference components."""
 
-from .model_manager import ModelManager
-from .inference_engine import InferenceEngine, ModelConfig
+from .model_manager import ModelManager, ModelConfig
+from .inference_engine import InferenceEngine
 
 __all__ = ['ModelManager', 'InferenceEngine', 'ModelConfig']
